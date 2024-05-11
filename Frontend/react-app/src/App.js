5f07f2601097185ec2083a8dcfe918245f8dbd5e
Pavan Kapoor <Pavankapoor31@gmail.com>
2024-05-11 19:21:36 +0530

    added signup and login pages

--- Frontend/react-app/src/App.js
+++ Frontend/react-app/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
+import RouteManager from './routes';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>GigHub</h1>
-      </header>
+    <div className='root-app'>
+
+      <RouteManager/>
+      <ToastContainer position="top-right" autoClose={2000} hideProgressBar={true} newestOnTop={true} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+
     </div>
+
   );
 }
 
---
+++ Frontend/react-app/src/Components/Login/Login.js
@@ -0,0 +1,90 @@
+// src/components/Login.js
+import { useState } from "react";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import {toast} from 'react-toastify'
+import axios from "axios";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        axios.get(
+          `http://localhost:3001/api/users?filter={"where":{"email":"${user.email}"}}`
+        ).then(
+          (res)=>{
+            try{
+              localStorage.setItem('profile.id',JSON.stringify(res.data[0].id))
+            }
+            catch (err){
+              console.log(err)
+            }
+          }
+        )
+       
+        localStorage.setItem('profile.email',JSON.stringify(user.email))
+        localStorage.setItem('stsTokenManager',JSON.stringify(user.stsTokenManager))
+        localStorage.setItem('displayName',JSON.stringify(user.displayName))
+        toast.success('Login successful.', { toastId: 'success', icon: false });
+        navigate('/home')
+        // ...
+      })
+      .catch((error) => {
+        toast.error('Login failed '+error, { toastId: 'failure', icon: true });
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        window.alert("Login Failed")
+      });
+      // User has been logged in successfully
+    } catch (error) {
+      console.error("Error logging in:", error.message);
+    }
+  };
+
+  return (
+    <div className=" content-wrapper d-flex justify-center align-center">
+      <div className="Auth-form-container mw-400 card p-4">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+                value={email} onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+                value={password} onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 w-100 mt-3">
+              <button type="button" className="btn btn-primary" onClick={handleLogin}>
+                Sign in
+              </button>
+            </div>
+            <p className="forgot-password text-right mt-2">
+              New here? <a href="#" onClick={()=>{navigate('/signup')}}>Sign up</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

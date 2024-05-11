34b2ef5b494f45d8078d7f857c4de6e40499ed74
VishalChaudharyHere <vishalchhere@gmail.com>
2024-05-12 00:10:59 +0530

    css for BioCard

--- Frontend/react-app/src/Components/Profile/BioCard/BioCard.js
+++ Frontend/react-app/src/Components/Profile/BioCard/BioCard.js
@@ -1,25 +1,65 @@
 import React from 'react'
+import './BioCard.css'
 
-export default function BioCard() {
+export default function BioCard({info}) {
 
 
   return (
-    <div>
-      <img src="" alt="" />
-      <div className='d-flex'>
-        <div></div>
-        <div></div>
+    <div className='d-flex flex-column border rounded-top'>
+      <div className='bio-card-container d-flex justify-content-between rounded-top'>
+        
+        <div className='p-2'>
+          <img src={require('../../../assets/images/IMG.gif')}
+              alt="Profile image"
+              className='border rounded-circle'
+              style={{width: '6rem', height: '6rem'}}
+          />
+        </div>
+
+        <div className='d-flex w-100 pt-2 pb-2 pl-3 pr-3'>
+          <div className='d-flex w-100 flex-column'>
+            <div className='d-flex font-sans justify-content-between align-items-center'>
+              <div className='h4'>{info.name}</div>
+              {/* <span>ICONS</span> */}
+            </div>
+
+            <div className='d-flex justify-content-between align-items-center gap-4'>
+              <div className='text-sm text-muted'>{info.headline}</div>
+              
+              <div className='d-flex gap-2'>
+                {/* <div className='d-flex flex-column justify-content-between align-items-center'>
+                  <span className='text-sm font-sans font-semibold'>Followers</span>
+                  <span className='text-xs'>{info.followers}</span>
+                </div>
+
+                <div className='d-flex flex-column justify-content-between align-items-center'>
+                  <span className='text-sm font-sans font-semibold'>Following</span>
+                  <span className='text-xs'>{info.following}</span>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
-      <div>
-        <div>
-            {/* icons */}
+      <div className='about d-flex flex-column p-2 align-items-start'>
+        <span className='text-md font-sans font-semibold'>About me</span>
+        <p className='text-xs text-slate-500'>{info.bio}{info.bio}</p>
+      </div>
+
+      <div className='about row'>
+        <div className='col-3 pb-2 d-flex flex-column'>
+          <span className='text-md font-semibold'>Location</span>
+          <span className='text-xs text-slate-500'>Melbourne, Australia</span>
         </div>
-        <div>
-            <div><span>Followers</span></div>
-            <span>Following</span>
+
+        <div className='col-7 pb-2'>
+            {/* <button>Btn</button> */}
         </div>
       </div>
+
+
     </div>
   )
 }
--- Frontend/react-app/src/Components/Profile/LancerProfile.js
+++ Frontend/react-app/src/Components/Profile/LancerProfile.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import BioCard from './BioCard/BioCard'
 
+const bioCardInfo = {
+  name: 'Reetik',
+  headline: 'Hi, I am a react developer!',
+  bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, nihil inventore. Error harum beatae laboriosam? Inventore facere non amet consequatur, corrupti ducimus! Nemo porro dicta excepturi impedit hic voluptatem at!',
+}
+
 export default function LancerProfile() {
   return (
-    <div>
-      <BioCard />
+    <div className='p-2 w-100'>
+      <BioCard info={bioCardInfo} />
     </div>
   )
 }

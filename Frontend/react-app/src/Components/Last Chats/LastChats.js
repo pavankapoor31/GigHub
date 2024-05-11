7237ecc5a0553cd1db2b5bbbf12257a64f82f62e
VishalChaudharyHere <vishalchhere@gmail.com>
2024-05-12 05:09:52 +0530

    last messages

--- Frontend/react-app/src/Components/Last Chats/LastChats.js
+++ Frontend/react-app/src/Components/Last Chats/LastChats.js
@@ -1,26 +1,12 @@
 import React from 'react'
 import'./lastchats.css'
-import ChatCard from './ChatCard'
 
 export default function LastChats() {
   return (
     <div className='d-flex flex-column fit-content border h-100'>
-      {/* <ChatCard />
       <ChatCard />
-      <ChatCard /> */}
-        <List>
-          {/* {messages.map(message => (
-            <ListItem key={message.id}>
-              <ListItemAvatar>
-                <Avatar alt={message.user.name} src={message.user.profilePic} className={classes.avatar} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={<Typography variant="subtitle1" component="div">{message.user.name}</Typography>}
-                secondary={message.text}
-              />
-            </ListItem>
-          ))} */}
-        </List>
+      <ChatCard />
+      <ChatCard />
     </div>
   )
 }

5ffc9289854c59e1d809ce6cec89656f61385e78
mahimabiswas <mahimabiswas28@gmail.com>
2024-05-12 14:28:57 +0530

    fix css

--- Frontend/react-app/src/Components/Messages/Messages.jsx
+++ Frontend/react-app/src/Components/Messages/Messages.jsx
@@ -1,98 +1,101 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import './Messages.css'; // Import the CSS file for styling
 import { TextField, Button } from '@mui/material';
 import { setIsSeller,setRole } from '../../redux/actions/gighub.actions';
 import { useSelector,useDispatch } from 'react-redux'
+import { BASE_URL } from '../../global_config';
+import axios from 'axios';
 function Messages() {
   const { messagerId } = useParams();
   const [profileId, setProfileId] = useState('');
-  const [profileUserName, setProfileUserName] = useState('');
-  const [isClient, setIsClient] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [messegerUserName, setMessagerUserName] = useState('');
-  const [userTypeIsSet, setUserTypeIsSet] = useState('');
   const [newMessage, setNewMessage] = useState('');
+  const [clientId, setClientId] = useState('');
+  const [freelancerId, setFreelancerId] = useState('');
   const isSeller = useSelector((state)=>state.gighubReducer.isSeller)
-  const freelancerId = useSelector((state)=>state.gighubReducer.freelancerId)
+  const role = useSelector((state)=>state.gighubReducer.role)
+  // const freelancerId = useSelector((state)=>state.gighubReducer.freelancerId)
   console.log(isSeller,'isSeller')
   console.log(freelancerId,"isSeller1234")
   useEffect(() => {
-    if(isSeller){
-      setProfileId(freelancerId)
+    if(role==='buyer'){
+      let storedProfileId = localStorage.getItem('client_data');
+      if (!storedProfileId) return;
+      console.log(storedProfileId,'storedProfileId')
+      storedProfileId = JSON.parse(storedProfileId);
+      storedProfileId = storedProfileId.id;
+      setProfileId(storedProfileId);
+      setClientId(storedProfileId);
+      setFreelancerId(messagerId);
     }
     else{
-      let storedProfileId = localStorage.getItem('profile.id');
-      setProfileId(JSON.parse(storedProfileId));
+      let storedProfileId = localStorage.getItem('freelancer_data');
+      if(!storedProfileId) return;
+      console.log(storedProfileId,'storedProfileId2')
+      storedProfileId = JSON.parse(storedProfileId)
+      storedProfileId = storedProfileId.id
+      setProfileId(storedProfileId);
+      setClientId(messagerId);
+      setFreelancerId(storedProfileId);
     }
+    console.log(clientId, freelancerId, profileId,'messagesdeets')
   }, []);
 
+  const freelancerIdRef = useRef();
+  const clientIdRef = useRef();
+  useEffect(
+    ()=>{
+      freelancerIdRef.current = freelancerId;
+      clientIdRef.current = clientId
+    },[clientId,freelancerId]
+  )
   const fetchMessages = async () => {
     try {
-      if (isSeller !== null && profileId !== null && messagerId!== null) {
-        const clientId = isSeller ? profileId : messagerId;
-        const freelancerId = isSeller ? messagerId : profileId;
-        const response = await fetch(`http://172.208.57.14:3001/api/messages?filter={"where":{"client_id":"${clientId}","freelancer_id":"${freelancerId}"},"order":"timestamp ASC"}`);
-        const data = await response.json();
-        setMessages(data);
-      }
+        if (clientId && freelancerId) {
+          axios.get(`${BASE_URL}/api/messages?filter={"where":{"client_id":"${clientId}","freelancer_id":"${freelancerId}"},"order":"timestamp ASC"}`).then(
+            (res)=>{
+              console.log(res.data,'res.data')
+              setMessages(res.data);
+            }
+          )
+          // setMessages((prev) => [...prev, data]);
+        } else {
+          console.error('profileId or messagerId is null');
+        }
     } catch (error) {
       console.error('Error fetching messages:', error);
     }
   };
+  
 
   useEffect(() => {
+    if (!profileId || !clientId ||   !freelancerId ) return;
     fetchMessages();
-    const intervalId = setInterval(fetchMessages, 10000);
+    const intervalId = setInterval(fetchMessages, 6000);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [clientId,freelancerId]);
 
   useEffect(() => {
-    if (profileId) {
-      fetch(`http://172.208.57.14:3001/api/freelancers?filter={"where":{"id":"${profileId}"}}`)
-        .then(response => response.json())
-        .then(data => {
-          if (data.length > 0) {
-            setIsClient(false);
-            setUserTypeIsSet(true);
-          } else {
-            fetch(`http://172.208.57.14:3001/api/clients?filter={"where":{"id":"${profileId}"}}`)
-              .then(response => response.json())
-              .then(data => {
-                if (data.length > 0) {
-                  setIsClient(true);
-                  setUserTypeIsSet(true);
-                }
-              })
-              .catch(error => console.error('Error fetching client:', error));
-          }
-        })
-        .catch(error => console.error('Error fetching freelancer:', error));
-    }
-  }, [profileId]);
-
-  useEffect(() => {
-    if (isSeller !== null && profileId !== null && messagerId!== null) {
-      const clientId = isSeller ? profileId : messagerId;
-      const freelancerId = isSeller ? messagerId : profileId;
-
-      fetch(`http://172.208.57.14:3001/api/messages?filter={"where":{"client_id":"${clientId}","freelancer_id":"${freelancerId}"}}`)
+    if (profileId !== null && clientId !== null && freelancerId!== null) {
+      fetch(`${BASE_URL}/api/messages?filter={"where":{"client_id":"${clientId}","freelancer_id":"${freelancerId}"},"order":"timestamp ASC"}`)
         .then(response => response.json())
         .then(data => setMessages(data))
         .catch(error => console.error('Error fetching messages:', error));
     }
-  }, [isSeller, profileId, messagerId]);
+    console.log(messages)
+  }, [profileId, clientId, freelancerId]);
   const sendMessage = async () => {
     try {
-      if (profileId && messagerId && newMessage.trim() !== '') {
-        const response = await fetch('http://172.208.57.14:3001/api/messages', {
+      if (profileId && clientId && freelancerId.trim() !== '') {
+        const response = await fetch(`${BASE_URL}/api/messages`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            client_id: isSeller ? profileId : messagerId,
-            freelancer_id: isSeller ? messagerId : profileId,
+            client_id: clientId,
+            freelancer_id: freelancerId,
             message: newMessage,
             timestamp: new Date().toISOString(),
             sentBy: profileId
@@ -100,6 +103,7 @@ function Messages() {
         });
         if (response.ok) {
           setNewMessage('');
+          // setMessages((prev) => [...prev, data]);
           fetchMessages();
         } else {
           console.error('Failed to send message');
@@ -110,12 +114,15 @@ function Messages() {
     }
   };
   return (
-    <div className="chat-container">
-      {messages.map(message => (
-        <div key={message.id} className={message.sentBy === profileId ? 'sent' : 'received'}>
-          <p>{message.message}</p>
-        </div>
-      ))}
+    <div className="chat-container position-absolute bottom-0 overflow-hidden">
+      <div className='d-flex flex-column h-100 w-100 overflow-auto'>
+        {messages.map(message => (
+          <div className={`d-flex align-items-center border ${message.sentBy === profileId ? 'sent' : 'received'}`} key={message.id}>
+            <p>{message.message}</p>
+          </div>
+        ))}
+      </div>
+      
       <div className="message-input">
       <TextField
           label="New Message"
